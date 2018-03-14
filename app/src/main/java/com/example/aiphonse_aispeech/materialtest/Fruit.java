1a4b6ede80a2f647bc3636285874771de8d770e5
Spike <xytx1989@vip.qq.com>
2018-03-14 20:26:43 +0800

    卡片排列+下拉刷新+悬浮+自动跳转

---
+++ app/src/main/java/com/example/aiphonse_aispeech/materialtest/Fruit.java
@@ -0,0 +1,24 @@
+package com.example.aiphonse_aispeech.materialtest;
+
+/**
+ * Created by AiPhonse_AISpeech on 3/14/2018.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+    public Fruit(String name, int imageId){
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+
+
+}
